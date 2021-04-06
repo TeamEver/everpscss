5a4b1df79f61a3538443daf92108ec2f5b415543
Cyssoo <cyrilchalamon@gmail.com>
2021-04-06 16:24:20 +0200

    Init repo, based on Bitbucket repo

---
+++ translations/fr.php
@@ -0,0 +1,25 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{everpscss}prestashop>everpscss_1373451fd31148c51eebdd1969a990c9'] = 'Ever CSS';
+$_MODULE['<{everpscss}prestashop>everpscss_0efbac3ffae395fd02d90ebbeb0fc139'] = 'Ajoutez votre code CSS et JS personnalisé';
+$_MODULE['<{everpscss}prestashop>everpscss_f4f70727dc34561dfde1a3c529b6205c'] = 'Réglages';
+$_MODULE['<{everpscss}prestashop>everpscss_42e6feb7f76e910efcda54034ecc83ce'] = 'CSS personnalisées';
+$_MODULE['<{everpscss}prestashop>everpscss_80a179cce95fdeb4fde0483f02347157'] = 'Ajoutez ici votre code CSS personnalisé';
+$_MODULE['<{everpscss}prestashop>everpscss_fde0ed3a9f1fc7c4f66e0df671e7d522'] = 'Les webdesigners peuvent ici gérer les CSS personnalisées';
+$_MODULE['<{everpscss}prestashop>everpscss_0bfe12be993fee0d3a848f1495e54916'] = 'Javascript personnalisé';
+$_MODULE['<{everpscss}prestashop>everpscss_4a679274db7ef566f7b8459700787542'] = 'Ajoutez ici vos règles personnalisées en javascript';
+$_MODULE['<{everpscss}prestashop>everpscss_f1c7a91d2c5316c1104ee82707c0baff'] = 'Les webdesigners peuvent ici ajouter leurs propres règles de javascript';
+$_MODULE['<{everpscss}prestashop>everpscss_c9cc8cce247e49bae79f15173ce97354'] = 'Enregistrer';
+$_MODULE['<{everpscss}prestashop>everpscss_03cfbbac21f2cbbfe16c39f57376be35'] = 'Les réglages ont bien été enregistrés';
+$_MODULE['<{everpscss}prestashop>footer_948e7853fa34f870e3e176a82ee6fc60'] = 'Ever CSS / JS';
+$_MODULE['<{everpscss}prestashop>footer_4223656c24e622122ceacfc60ce9df65'] = 'Merci pour votre confiance :-)';
+$_MODULE['<{everpscss}prestashop>footer_672713b9ebcaa2c438970dd65b8a49b5'] = 'N\'hésitez pas à nous contacter pour du support ou de l\'aide';
+$_MODULE['<{everpscss}prestashop>header_948e7853fa34f870e3e176a82ee6fc60'] = 'Ever CSS / JS';
+$_MODULE['<{everpscss}prestashop>header_5c96d1c087f388287fa091121b00627b'] = 'Merci d\'utiliser les modules de Team Ever';
+$_MODULE['<{everpscss}prestashop>header_c4d2324eea35c7e7b51b75f861e319dd'] = 'Comment être le premier sur Google ?';
+$_MODULE['<{everpscss}prestashop>header_923577b3c4bd51ca4c20083fd149c692'] = 'Nous avons créé le meilleur module de référencement, en travaillant avec d\'énormes sites Web et sociétés de référencement';
+$_MODULE['<{everpscss}prestashop>header_4155fbd49af1be337176d442a5ebdeb0'] = 'Voir le meilleur module de référencement sur Prestashop Addons';
+$_MODULE['<{everpscss}prestashop>upgrade_11f4d2cc4546c08ed99645473ba29a5f'] = 'Une mise à jour est disponible sur notre boutique. Vérifiez s\'il vous plaît';
+$_MODULE['<{everpscss}prestashop>upgrade_a43640b6c7cabe3d8d157aba85f7cee4'] = 'afin d\'obtenir la dernière version du module';
---
+++ views/templates/admin/header.tpl
@@ -0,0 +1,22 @@
+{*
+* Project : everpscss
+* @author Team EVER
+* @copyright Team EVER
+* @license   Tous droits réservés / Le droit d'auteur s'applique (All rights reserved / French copyright law applies)
+* @link https://www.team-ever.com
+*}
+
+<div class="panel row">
+    <h3><i class="icon icon-smile"></i> {l s='Ever CSS / JS' mod='everpscss'}</h3>
+    <div class="col-md-6">
+        <img id="everlogo" src="{$everpscss_dir|escape:'htmlall':'UTF-8'}logo.png" style="max-width: 120px;">
+        <p>{l s='Thanks for using Team Ever\'s modules' mod='everpscss'}.<br /></p>
+    </div>
+    <div class="col-md-6">
+        <h4>{l s='How to be first on Google pages ?' mod='everpscss'}</h4>
+        <p>{l s='We have created the best SEO module, by working with huge websites and SEO societies' mod='everpscss'}</p>
+        <p>
+            <a href="https://addons.prestashop.com/fr/seo-referencement-naturel/39489-ever-ultimate-seo.html" target="_blank">{l s='See the best SEO module on Prestashop Addons' mod='everpscss'}</a>
+        </p>
+    </div>
+</div>
